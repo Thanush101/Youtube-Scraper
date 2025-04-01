4ed8b9299874d05e77e0b91022f3f2c0cb007584
Thanush <115856620+Thanush101@users.noreply.github.com>
2025-04-01 17:37:08 +0530

    Update start script with environment variables

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-# Start the application using gunicorn
+# Set environment variables
+export RENDER=true
+export PLAYWRIGHT_BROWSERS_PATH=/tmp/pw-browsers
+
+# Start the application
 gunicorn app:app --bind 0.0.0.0:$PORT 
